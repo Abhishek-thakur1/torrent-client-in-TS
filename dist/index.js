fe1926df124a194fe188ffc29383e71b074bc345
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-06-26 20:44:38 +0530

    added index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
-let message = "Hello, World!";
-console.log(message);
+const fs = require('fs');
+const bencode = require('bencode');
+const parseTorrent = require('parse-torrent');
+let args = process.argv.slice(2);
+let torrentFileName = args[0];
+if (typeof (torrentFileName) !== 'undefined') {
+    let torrentFile = fs.readFileSync(torrentFileName);
+    let torrent = bencode.decode(torrentFile);
+    let pt = parseTorrent(torrentFile);
+    let remaining = pt.pieces.length;
+    console.log(torrentFile);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-let message: string = "Hello, World!";
-console.log(message);
+const fs = require('fs');
+const bencode = require('bencode');
+const parseTorrent = require('parse-torrent');
+
+let args = process.argv.slice(2);
+let torrentFileName: string = args[0];
+
+if (typeof (torrentFileName) !== 'undefined') {
+    let torrentFile = fs.readFileSync(torrentFileName);
+    let torrent = bencode.decode(torrentFile)
+    let pt = parseTorrent(torrentFile)
+    let remaining: number = pt.pieces.length
+
+    console.log(torrentFile)
+}
+
