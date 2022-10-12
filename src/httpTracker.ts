1cf96554ee67d8a83e91994f448589b0c854c496
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-10-12 23:24:48 +0530

    re:

--- src/httpTracker.ts
+++ src/httpTracker.ts
@@ -3,14 +3,14 @@ const parseTorrentFile = require("parse-torrent");
 const bencodeHttp = require("bencode");
 const request = require("request");
 const peer = require("./peer");
-import {Peer} from './types'
+import { Peer } from './types'
 
 let j: number = 0;
 
-function getPeerList(torrent: any, callback: any): void{
-    
+function getPeerList(torrent: any, callback: any): void {
+
     // creating and sending announce request...
-    
+
     let pt = parseTorrentFile(bencodeHttp.encode(torrent))
 
     // make request if tracker list not exhausted
@@ -20,16 +20,16 @@ function getPeerList(torrent: any, callback: any): void{
             url: pt.announce[j] + getAnnounceQueryString(torrent),
             method: 'GET',
             encoding: null
-        }, function(error: any, response: any, body: any) {
+        }, function (error: any, response: any, body: any) {
             if (error) {
-                console.log("Error requesting from tracker:"+pt.announce[j]);
+                console.log("Error requesting from tracker:" + pt.announce[j]);
                 console.log("Trying next tracker")
                 //try next tracker
                 j++;
                 getPeerList(torrent, callback);
             }
             else {
-                console.log("Tracker response:"+body);
+                console.log("Tracker response:" + body);
                 var decodedBody = bencodeHttp.decode(body);
                 if (decodedBody['failure reason']) {
                     console.log(response.statusCode, "error: " + bencodeHttp.encode(decodedBody['failure reason']));
@@ -52,7 +52,7 @@ function getPeerList(torrent: any, callback: any): void{
                     }
                     else {
                         console.log("Normal Notation");
-                        peerList = decodedBody.peers.map(function(peer: any) {
+                        peerList = decodedBody.peers.map(function (peer: any) {
                             return {
                                 ip: peer.ip.toString("utf8"),
                                 port: peer.port
@@ -76,13 +76,13 @@ function getAnnounceQueryString(torrent: any): string {
     var announceRequest = "?info_hash=" + escapeInfoHash(pTorrent.infoHash) +
         "&peer_id=-VT0001-000000000000&port=6687&uploaded=0&downloaded=0&left=" +
         pTorrent.length + "&event=started";
-    
+
     return announceRequest;
 }
 
 function escapeInfoHash(infoHash: any): any {
     var h = infoHash;
-    h = h.replace(/.{2}/g, function(m: string) {
+    h = h.replace(/.{2}/g, function (m: string) {
         var v = parseInt(m, 16);
         if (v <= 127) {
             m = encodeURIComponent(String.fromCharCode(v));
